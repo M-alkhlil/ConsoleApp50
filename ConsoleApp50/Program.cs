53eef44d903be62cc1acb11785647d4316cf43b3
alkhlil <alkhlil@LAPTOP-E4CGAQ96>
2024-01-15 11:22:44 +0800

    Add project files.

---
+++ ConsoleApp50/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello.program
+{
+    internal class Program
+    {
+        public class Car1
+        {
+            public string color1 = "Red";
+        }
+        public class Car2
+        {
+            public string color2 = "Blue";
+        }
+        static void Main(string[] args)
+        {
+            Car1 obj = new Car1();
+            Car2 obj2 = new Car2();
+            Console.WriteLine(obj.color1);
+            Console.WriteLine(obj2.color2);
+
+        }
+
+    }
+}
