05d1ea3d69c3fd7431dd954261e385cd030edfba
alkhlil <alkhlil@LAPTOP-E4CGAQ96>
2024-01-15 11:25:07 +0800

    add new class

--- ConsoleApp50/Program.cs
+++ ConsoleApp50/Program.cs
@@ -16,12 +16,19 @@ namespace Hello.program
         {
             public string color2 = "Blue";
         }
+        public class Car3
+        {
+            public string color3 = "Black";
+        }
         static void Main(string[] args)
         {
             Car1 obj = new Car1();
             Car2 obj2 = new Car2();
+            Car3 obj3 = new Car3();
+
             Console.WriteLine(obj.color1);
             Console.WriteLine(obj2.color2);
+            Console.WriteLine(obj3.color3);
 
         }
 
